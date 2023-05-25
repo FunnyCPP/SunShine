93b437e7e4cf7e400d7d35c58a6a525df28791c9
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-25 10:40:54 +0200

    Started implementing filters. First created authentication Header, when we need make a call  we need to the pass the JWT auth token

--- authorization-service/src/main/java/com/sggw/authorizationservice/config/JwtAuthenticationFilter.java
+++ authorization-service/src/main/java/com/sggw/authorizationservice/config/JwtAuthenticationFilter.java
@@ -18,6 +18,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain)
             throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+
 
     }
 }
