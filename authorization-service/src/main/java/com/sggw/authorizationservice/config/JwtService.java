86b92a7ebfbd871577c0edaf00678abdcb3fc5ea
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-26 11:39:00 +0200

    Created method Claims take a string as a parameter.  We need return our jwts from the io.json web a few annotation. Also created signing key used to created the signature of the jwt token which is used to verify that the sender of the jwt who is

--- authorization-service/src/main/java/com/sggw/authorizationservice/config/JwtService.java
+++ authorization-service/src/main/java/com/sggw/authorizationservice/config/JwtService.java
@@ -1,10 +1,31 @@
 package com.sggw.authorizationservice.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
-    public String extractUsername(String jwt) {
+
+    private static final  String SECRET_KEY = "645267556A586E3272357538782F413F4428472B4B6250655368566D59703373";
+    public String extractUsername(String token) {
         return null;
+    }
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
