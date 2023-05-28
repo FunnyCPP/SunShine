308809f12fb1ea06edcd575ef487ad46c8d4dcad
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-28 09:01:18 +0200

    remove not used imports

--- authorization-service/src/main/java/com/sggw/authorizationservice/config/ApplicationConfig.java
+++ authorization-service/src/main/java/com/sggw/authorizationservice/config/ApplicationConfig.java
@@ -1,11 +1,9 @@
 package com.sggw.authorizationservice.config;
 
-import com.sggw.authorizationservice.user.User;
 import com.sggw.authorizationservice.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
