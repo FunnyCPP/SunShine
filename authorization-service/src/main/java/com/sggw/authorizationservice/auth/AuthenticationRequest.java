cb9f7a4764d6a578d45c862ebcbba059e349d8dc
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-29 10:15:51 +0200

    Implemented register method will allow us to create a user save it to the database and return generated token out

--- authorization-service/src/main/java/com/sggw/authorizationservice/auth/AuthenticationRequest.java
+++ authorization-service/src/main/java/com/sggw/authorizationservice/auth/AuthenticationRequest.java
@@ -13,6 +13,4 @@ public class AuthenticationRequest {
 
     private String email;
     private String password;
-
-
 }
--- authorization-service/src/main/java/com/sggw/authorizationservice/auth/AuthenticationService.java
+++ authorization-service/src/main/java/com/sggw/authorizationservice/auth/AuthenticationService.java
@@ -1,17 +1,34 @@
 package com.sggw.authorizationservice.auth;
 
+import com.sggw.authorizationservice.config.JwtService;
+import com.sggw.authorizationservice.user.Role;
+import com.sggw.authorizationservice.user.User;
+import com.sggw.authorizationservice.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .role(Role.USER)
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
-
-
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         return null;
     }
