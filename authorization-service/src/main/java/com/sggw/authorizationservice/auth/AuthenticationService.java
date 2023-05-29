39ccf0813b0df901450a82cc7715a9ad102d4e28
Darek <106867377+Darek100100@users.noreply.github.com>
2023-05-29 10:33:58 +0200

    Implemented authenticate method

--- authorization-service/src/main/java/com/sggw/authorizationservice/auth/AuthenticationService.java
+++ authorization-service/src/main/java/com/sggw/authorizationservice/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.sggw.authorizationservice.user.Role;
 import com.sggw.authorizationservice.user.User;
 import com.sggw.authorizationservice.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
@@ -30,6 +33,17 @@ public class AuthenticationService {
                 .build();
     }
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
